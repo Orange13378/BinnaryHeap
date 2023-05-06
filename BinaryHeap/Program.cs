c86971535e2b64c3549ef36df17cac4337a2885e
Orange13378 <muslim_askerli@mail.ru>
2023-05-06 10:55:07 +0300

    Добавьте файлы проекта.

---
+++ BinaryHeap/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BinaryHeap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var startItems = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                startItems.Add(random.Next(-1000, 1000));
+            }
+
+            var heap = new Heap(startItems);
+
+            for (int i = 0; i < 100; i++)
+            {
+                heap.Add(random.Next(-1000, 1000));
+            }
+
+            foreach (var item in heap)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
